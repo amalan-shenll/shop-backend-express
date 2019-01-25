56bee545663edf41b9bfc7d0f8b36ef261184574
Amalan Jenicious <amalan.f@shenll.com>
2019-01-25 19:56:13 +0530

    Created endpoint for user registration

--- src/user.js
+++ src/user.js
@@ -72,5 +72,91 @@ router.post('/auth',(req, res) => {//creating endpoint for user authentication
   }
 });
 
+//api endpoint for user registration
+router.post('/register', (req, res) => {
+  let data = req.body;
+  let email = data.email || "";
+  let password = data.password || "";
+
+  var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; //regex for email validation
+
+  if(!email) {
+    res.json({
+      status: "error",
+      code: "email_required",
+      message: "Email must not be empty."
+    });
+  } else if(!emailRegex.test(email)) {//validating email with regex pattern
+    res.json({
+      status: "error",
+      code: "invalid_email",
+      message: "Please give valid email."
+    });
+  } else if(!password) {
+    res.json({
+      status: "error",
+      code: "password_required",
+      message: "Password must not be empty."
+    });
+  } else {
+    Users.findOne({email}, (err, user) => {//getting user detail for given email
+      if(err) {
+        res.json({
+          status: "error",
+          code: "server_error",
+          message: "Error retrieving user details."
+        });
+      } else if(_.isEmpty(user)){//will be empty if user not present already in db with given mail 
+        bcrypt.hash(password, saltRounds, (err, hash) => {//generating password hash
+          if(err) {
+            res.json({
+              status: "error",
+              code: "server_error",
+              message: "Error processing user password."
+            });
+          } else {
+            let authToken = randtoken.generate(16);//generating authtoken
+            /*
+            Note:
+              authtoken will be used to validated user session on api calls
+            */
+            let newUser = new Users({//creating new user
+              email,
+              password: hash,
+              authToken
+            });
+            newUser.save((err, user) => {//saving new user to db
+              if(err) {
+                res.json({
+                  status: "error",
+                  code: "server_error",
+                  message: "Error creating new user."
+                });
+              } else {
+                /*
+                  if user created successfully response with authtoken
+                */
+                res.json({
+                  status: "success",
+                  code: "user_created_success",
+                  message: "User created successfully.",
+                  role: user.role,
+                  authToken
+                });
+              }
+            });
+          }
+        });
+      } else {//if user not empty means user with given email already present
+        res.json({
+          status: "error",
+          code: "user_already_present",
+          message: "User with " + email + " already present."
+        });
+      }
+    });
+  }
+});
+
 module.exports = router;
 
